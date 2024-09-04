20d534699aaae3663346f0a728db144d451f3bf4
zhangcheng.liu <zhangcheng.liu@lkcoffee.com>
2024-09-04 18:22:01 +0800

    feat: skw-spring-boot-starter-common

---
+++ skw-framework/skw-spring-boot-starter-common/src/main/java/com/skw/saas/framework/common/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @Description: 基础的通用类
+ * @Author: zhangcheng.liu
+ * @Date: 2024/9/4
+ */
+package com.skw.saas.framework.common;
---
+++ skw-framework/skw-spring-boot-starter-common/src/main/java/com/skw/saas/framework/common/validation/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 使用 Hibernate Validator 实现参数校验
+ */
+package com.skw.saas.framework.common.validation;
